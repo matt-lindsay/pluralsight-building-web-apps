46bc39e863fb372e4d32f44e64202baf369b0570
matt-lindsay <matt_epilp@hotmail.com>
2018-09-27 14:29:42 +0100

    Rendering.

--- app.js
+++ app.js
@@ -16,9 +16,31 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  }];
+
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('Hello books');
+    res.render('books',
+      {
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }
+        ],
+        title: 'Library',
+        books
+      });
   });
 
 bookRouter.route('/single')
